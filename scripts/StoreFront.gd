b284e54f7fdd5bbebb1d03b168982ecebfcd2928
montinevra <montinevra@gmail.com>
2020-02-02 14:07:29 -0700

    'final' build for gamejam

--- scripts/StoreFront.gd
+++ scripts/StoreFront.gd
@@ -5,8 +5,14 @@ extends Node
 # var b = "text"
 onready var m_dialog_customer = get_node("DialogCustomer")
 onready var m_dialog_player = get_node("DialogPlayer")
+onready var m_repairable_sprite = get_node("Bg/tv_broken")
 #onready var m_score_label = get_node("Bg/Score/ScoreLabel")
 var m_score = 0
+
+
+func set_repairable_texture(t_texture):
+	m_repairable_sprite.set_texture(t_texture)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
